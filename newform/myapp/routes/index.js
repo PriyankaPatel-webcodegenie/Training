cb3ea9e547e4cedd66e0ecce5df9aab247a45455
PRIYANKA PATEL <88400072+PriyankaPatel-webcodegenie@users.noreply.github.com>
2021-11-16 19:26:45 +0530

    AJAX searching

--- newform/myapp/routes/index.js
+++ newform/myapp/routes/index.js
@@ -2,94 +2,100 @@ var express = require('express');
 var router = express.Router();
 var userModel =require('../models/user_model');
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
 
-
-  userModel.find(function (err, data) {
-    if (err) {
-      console.log("error in fetch data");
-    }
-    else {
+  try{
+    await userModel.find( function (err, data) {
       console.log("data fetch sucessfully");
       res.render('index', { user_array: data });
-    }
-  }).lean()
-});
-router.get('/users', function(req, res, next) {
+    
+    }).lean()
+  }
+  catch(err){
+  console.log(err)
+  }
 
-
-  userModel.find(function (err, data) {
-    if (err) {
-      console.log("error in fetch data");
-    }
-    else {
-      console.log("data fetch sucessfully");
-      res.render('partials/display', { user_array: data });
-    }
-  }).lean()
 });
-router.post('/', function (req, res, next) {
+router.get('/users', async function(req, res, next) {
+  try{
+    let condition = {};
+     if(req.query.search){
+   // let data = userModel.find( { $text: { $search: req.query.search } } )
+
   
- var myfile = req.files.photo; 
-   var myfilename = req.files.photo.name;
-  // var myfilename = null;
-   const mybodydata = {
-     firstname:req.body.firstname,
-     lastname:req.body.lastname,
-     address:req.body.address,
-     interest:req.body.interest,
-     hobby:req.body.hobby,
-     gender:req.body.gender,
-     photo:myfilename
+
+        // let find = userModel.find({$or: [{firstname: req.query.search},{lastname:req.query.search}]})
+    
+condition={$or: [{firstname: req.query.search},{lastname:req.query.search}]},{address:req.query.search}
+      // console.log(req.query)
+       // condition.firstname = req.query.search;
+      // console.log("search")
+      // console.log(condition.firstname)
+     }
+    if(req.query.formgender){
+           condition.gender = req.query.formgender;
+      console.log("gender is...")
+      console.log(condition.gender)
     }
-    var data = userModel(mybodydata);
-    data.save(function(err){
-      if(err){
-          console.log("Error in Insert user"+err);
-      }else{
-        myfile.mv("public/upload/"+myfilename,function(err){
-          if(err){
-            return res.status(500).send(err);
-          }
-            else{
-              res.json(data);
-            }
-          console.log("file uploaded");
-        });
-          console.log("user Insert Successfully"+ data);
-          
-              //res.send('successfull'); 
-              //res.json(data);
-      }
-  })
+    console.log("hello search data is")
+    console.log(condition)
+    let data = await userModel.find(condition).lean()
+ 
+    res.render('partials/display', { user_array: data });
+  }
+  catch(err){
+   console.log(err);
+  }
+
+});
+router.post('/', async function (req, res, next) {
+  try{
+    var myfile = req.files.photo; 
+    var myfilename = req.files.photo.name;
+   // var myfilename = null;
+    const mybodydata = {
+      firstname:req.body.firstname,
+      lastname:req.body.lastname,
+      address:req.body.address,
+      interest:req.body.interest,
+      hobby:req.body.hobby,
+      gender:req.body.gender,
+      photo:myfilename
+     }
+     var data = await userModel(mybodydata);
+     data.save( function(err){
+      myfile.mv("public/upload/"+myfilename,function(err){
+        res.json(data);
+        console.log("file uploaded");
+      });
+        console.log("user Insert Successfully"+ data);
+        
+            //res.send('successfull'); 
+            //res.json(data);
+   })
+  }
+  catch(err){
+res.json(error)
+  }
+ 
  });
- router.delete('/:id', function(req, res, next) {
+ router.delete('/:id', async function(req, res, next) {
   var deleteid = req.params.id;
-  userModel.findByIdAndDelete(deleteid,function(err,data){
-    if(err)
-    {
-      console.log("Error in Delete " + err);
-    }else{
-      console.log("Record Deleted " + deleteid);
-      res.json({status:"data delete"});
-   //  res.json(deleteid)
-    }
+ await userModel.findByIdAndDelete(deleteid,function(err,data){
+  console.log("Record Deleted " + deleteid);
+  res.json({status:"data delete"});
   })
   res.render('index');
 });
-router.get('/edit/:id', function(req, res, next) {
+router.get('/edit/:id', async function(req, res, next) {
   var editid = req.params.id;
-  userModel.findById(editid,function(err,data){
-    if(err){
-      console.log("Error in Edit" + err)
-    }else{
-      console.log(data);
-      res.json(data)
-    }
+ await userModel.findById(editid,function(err,data){
+  console.log(data);
+  res.json(data)
   }).lean();
 
 });
-router.put('/edit/:id', function(req, res, next) {
+router.put('/edit/:id', async function(req, res, next) {
   console.log("PUT METHOD...")
   console.log("Edit" + req.params.id)
   var myfile = req.files.photo; 
@@ -106,15 +112,19 @@ router.put('/edit/:id', function(req, res, next) {
     photo:myfilename
   }
 
-  userModel.findByIdAndUpdate(editid,mybodydata,function(err,data){
-    if(err){
-      console.log("Error in Edit" + err)
-    }else{
-      console.log( "Record Updated" +  data);
-      res.json(data);
-     
-    }
+  await userModel.findByIdAndUpdate(editid,mybodydata,function(err,data){
+    console.log( "Record Updated" +  data);
+    res.json(data);
+   
   }).lean();
 
 });
+// router.get('/search/:query', async function(req, res, next) {
+//   var editid = req.params.id;
+//  await userModel.findById(editid,function(err,data){
+//   console.log(data);
+//   res.json(data)
+//   }).lean();
+
+// });
 module.exports = router;
